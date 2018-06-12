7135d66287dcc79af10312b7bd747379a69b84af
yameste <yameste@yahoo.co.uk>
2018-06-12 16:34:03 +0100

    removed errors has

--- resources/assets/js/views/Cards/CardRegister.vue
+++ resources/assets/js/views/Cards/CardRegister.vue
@@ -13,18 +13,14 @@
                             <div class="row justify-content-center">
                                 <div class="col-md-6">
                                     <input name="name"  v-validate="'required|min:6'" type="text" class="form-control" placeholder="Name" v-model="name" autocomplete="nope">
-                                    <p class="help is-danger" v-show="errors.has('name')">
-                                        {{ errors.first('name') }}
-                                    </p>
+                                    
                                 </div><!-- /.col-md-6 -->
                            
                         
                            
                                 <div class="col-md-6">
                                     <input name="address"  v-validate="'required|min:10'" type="text" class="form-control" placeholder="Address" v-model="address" autocomplete="nope">
-                                    <p class="help is-danger" v-show="errors.has('address')">
-                                        {{ errors.first('address') }}
-                                    </p>
+                                    
                                 </div><!-- /.col-md-6 -->
                             </div><!-- /.row -->
                             
@@ -34,36 +30,28 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <input name="home_phone"  v-validate="'required|numeric'" type="text" class="form-control" placeholder="Home Phone" v-model="home_phone" autocomplete="nope">
-                                    <p class="help is-danger" v-show="errors.has('home_phone')">
-                                        {{ errors.first('home_phone') }}
-                                    </p>
+                                    
                                 </div><!-- /.col-md-6 -->
                             
     
                             
                                 <div class="col-md-6">
                                     <input name="mobile_phone"  v-validate="'required|numeric'" type="text" class="form-control" placeholder="Mobile Phone" v-model="mobile_phone" autocomplete="nope">
-                                    <p class="help is-danger" v-show="errors.has('mobile_phone')">
-                                        {{ errors.first('mobile_phone') }}
-                                    </p>
+                                   
                                 </div><!-- /.col-md-6 -->
                             </div><!-- /.row -->
     
                             <div class="row">
                                 <div class="col-md-6">
                                     <input name="email"  v-validate="'required|email'" type="email" class="form-control" placeholder="Email" v-model="email" autocomplete="nope">
-                                    <p class="help is-danger" v-show="errors.has('email')">
-                                        {{ errors.first('email') }}
-                                    </p>
+                                    
                                 </div><!-- /.col-md-6 -->
                             
     
                           
                                 <div class="col-md-6">
                                     <input name="balance"  v-validate="'required|decimal'" type="text" class="form-control" placeholder="Balance" v-model="balance" autocomplete="nope">
-                                    <p class="help is-danger" v-show="errors.has('balance')">
-                                        {{ errors.first('balance') }}
-                                    </p>
+                                    
                                 </div><!-- /.col-md-6 -->
                             </div><!-- /.row -->
     
